24b488c777aeb5d7f83c41bb4584427e916e795b
oseasmoreto <oseas_moreto@hotmail.com>
2022-12-29 09:32:37 -0300

    Adiciona o stitches para estilizacao a aplicacao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
+import { styled, theme } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$green500',
+  span: {
+    fontWeight: 'bold'
+  },
+  '&:hover': {
+    backgroundColor: '#fff'
+  }
+})
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button><span>Teste</span> Enviar</Button>
   )
 }
