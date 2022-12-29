16b5088358471c5ad5c0d479b9e926e31c1f4581
oseasmoreto <oseas_moreto@hotmail.com>
2022-12-29 12:09:05 -0300

    Adiciona componente de produto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,27 @@
-import { styled, theme } from "../styles"
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  span: {
-    fontWeight: 'bold'
-  },
-  '&:hover': {
-    backgroundColor: '#fff'
-  }
-})
+import image01 from '../assets/products/01.png'
+import image02 from '../assets/products/02.png'
+import image03 from '../assets/products/03.png'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button><span>Teste</span> Enviar</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={image01} alt="Produto 01" width={520} height={480} />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$70,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={image02} alt="Produto 02" width={520} height={480} />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$70,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
