bf9a84a12e36093344284ac7fe66b13f1bb61b12
oseasmoreto <oseas_moreto@hotmail.com>
2022-12-30 11:05:03 -0300

    Adiciona chamada do produto por api

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,24 +1,59 @@
 import Image from "next/image"
-import { useRouter } from "next/router"
+import { GetStaticProps } from "next"
+
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
-import imgProduct from '../../assets/products/01.png'
+import Stripe from "stripe"
+import { stripe } from "../../lib/stripe"
+import { priceFormatter } from "../../utils/formatter"
 
-export default function Product() {
-  const { query } = useRouter()
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  }
+}
 
+export default function Product({ product } : ProductProps) {
+  
   return (
     <ProductContainer>
       <ImageContainer>
-        <Image src={imgProduct} alt="" />
+        <Image src={product.imageUrl} alt={product.name} />
       </ImageContainer>
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quo voluptas aliquid eum rem praesentium tempore earum? Omnis voluptatum quos quaerat architecto, reprehenderit, eius doloribus voluptatibus temporibus laborum nemo non.</p>
+        <p>{product.description}</p>
         <button>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, {id: string}> = async ({ params }) => {
+  const productId = params !== undefined ? params.id : ''
+
+  const product = await stripe.products.retrieve(productId,{
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: priceFormatter.format(price.unit_amount !== null ? price.unit_amount / 100 : 0),
+        description: product.description
+      }
+    },
+    revalidate: 60 * 60 * 1,
+  }
 }
