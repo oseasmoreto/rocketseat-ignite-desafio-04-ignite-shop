be8409a95fe8ee61399494384d2d85de486eeb3d
oseasmoreto <oseas_moreto@hotmail.com>
2022-12-30 10:49:35 -0300

    Adiciona layout da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,24 @@
+import Image from "next/image"
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
+import imgProduct from '../../assets/products/01.png'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return (<h1>Produto: {query.id}</h1>)
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={imgProduct} alt="" />
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quo voluptas aliquid eum rem praesentium tempore earum? Omnis voluptatum quos quaerat architecto, reprehenderit, eius doloribus voluptatibus temporibus laborum nemo non.</p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
